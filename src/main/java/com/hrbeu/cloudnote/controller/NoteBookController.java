d309fbe568265dee0ced0ae8cdcb0bdbb5a6e525
ewoifuoi <hamimelons@126.com>
2023-07-11 14:58:40 +0800

    merged

--- src/main/java/com/hrbeu/cloudnote/controller/NoteBookController.java
+++ src/main/java/com/hrbeu/cloudnote/controller/NoteBookController.java
@@ -6,10 +6,7 @@ import com.hrbeu.cloudnote.pojo.Note;
 import com.hrbeu.cloudnote.pojo.Notebook;
 import com.hrbeu.cloudnote.service.NoteBookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
